12522a1f15d043ef260cba2f797d52a869278c83
Deseteral <deseteral@gmail.com>
2018-01-14 11:26:23 +0100

    React router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import RecipeSuggesterPage from '../RecipeSuggesterPage/RecipeSuggesterPage';
 import styles from './App.css';
+
+const DEFAULT_ROUTE = '/suggester';
 
 function App(props) {
   return (
     <Provider store={props.store}>
-      <div className={styles['app-container']}>
-        <h1 className={styles['title']}>Co zjeść?</h1>
-        <div className={styles['container']}>
-          <RecipeSuggesterPage />
+      <BrowserRouter>
+        <div>
+          <Route
+            path="/"
+            exact
+            component={(() => <Redirect to={DEFAULT_ROUTE} />)}
+          />
+          <div className={styles['app-container']}>
+            <h1 className={styles['title']}>Co zjeść?</h1>
+            <div className={styles['container']}>
+              <Switch>
+                <Route path="/suggester" component={RecipeSuggesterPage} />
+              </Switch>
+            </div>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     </Provider>
   );
 }
