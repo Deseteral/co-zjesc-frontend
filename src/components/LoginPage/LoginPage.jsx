7c75d782881a3ea6cc7aa0965a695c2f5eafd7cd
Deseteral <deseteral@gmail.com>
2018-01-28 11:57:00 +0100

    Login page styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -16,28 +16,39 @@ class LoginPage extends Component {
     this.setState({ [part]: event.target.value });
   }
 
+  handleSubmit(event) {
+    const { username, password } = this.state;
+    console.log(username, password);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className={css(commonStyles['card'])}>
+      <div className={css(commonStyles['card'], styles['card'])}>
         <form
-          className={styles['card']}
+          className={styles['form']}
           action={`${SERVICE_URL}/token`}
           method="POST"
+          onSubmit={e => this.handleSubmit(e)}
         >
           <input
+            className={styles['input']}
             name="username"
             value={this.state.login}
+            placeholder="Nazwa użytkownika"
             onChange={e => this.handleChange(e, 'username')}
             type="text"
           />
           <input
+            className={styles['input']}
             name="password"
             value={this.state.password}
+            placeholder="Hasło"
             onChange={e => this.handleChange(e, 'password')}
             type="password"
           />
           <input type="hidden" name="grant_type" value="password" />
-          <input type="submit" value="Login" />
+          <input className={styles['button']} type="submit" value="Zaloguj" />
         </form>
       </div>
     );
