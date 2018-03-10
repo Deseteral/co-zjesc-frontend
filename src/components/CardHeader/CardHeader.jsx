34fd28c922e95a4aa862935778f955035307eb1c
Deseteral <deseteral@gmail.com>
2018-03-10 19:31:28 +0100

    Sections and map to json

--- src/components/CardHeader/CardHeader.jsx
+++ src/components/CardHeader/CardHeader.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CardHeader.css';
 
-function CardHeader({ children }) {
-  return (
-    <h1 className={styles['header']}>
-      {children}
-    </h1>
+function CardHeader({ children, secondary }) {
+  return React.createElement(
+    secondary ? 'h2' : 'h1',
+    { className: styles['header'] },
+    children,
   );
 }
 
 CardHeader.propTypes = {
   children: PropTypes.node.isRequired,
+  secondary: PropTypes.bool,
+};
+
+CardHeader.defaultProps = {
+  secondary: false,
 };
 
 export default CardHeader;
--- src/pages/RecipeEditPage/RecipeEditPage.jsx
+++ src/pages/RecipeEditPage/RecipeEditPage.jsx
@@ -3,6 +3,16 @@ import RichTextEditor from 'react-rte';
 import Card from '../../components/Card/Card';
 import CardHeader from '../../components/CardHeader/CardHeader';
 import TextField from '../../components/TextField/TextField';
+import Button from '../../components/Button/Button';
+
+function mapStateToJson(state) {
+  const { title } = state;
+
+  return {
+    title,
+    description: state.description.toString('markdown'),
+  };
+}
 
 class RecipeEditPage extends Component {
   constructor(props) {
@@ -13,20 +23,43 @@ class RecipeEditPage extends Component {
     };
   }
 
+  handleChange(value, part) {
+    this.setState({ [part]: value });
+  }
+
   render() {
     return (
       <Card>
         <CardHeader>Dodaj nowy przepis</CardHeader>
-        <TextField
-          name="title"
-          value={this.state.title}
-          placeholder="Tytuł przepisu"
-          onChange={value => this.handleChange(value, 'title')}
-        />
-        <RichTextEditor
-          value={this.state.description}
-          onChange={value => this.setState({ description: value })}
-        />
+        <section>
+          <TextField
+            name="title"
+            value={this.state.title}
+            placeholder="Tytuł przepisu"
+            onChange={value => this.handleChange(value, 'title')}
+          />
+        </section>
+        <section>
+          <CardHeader secondary>Zdjęcia</CardHeader>
+        </section>
+        <section>
+          <CardHeader secondary>Składniki</CardHeader>
+        </section>
+        <section>
+          <CardHeader secondary>Opis</CardHeader>
+          <RichTextEditor
+            value={this.state.description}
+            onChange={value => this.setState({ description: value })}
+          />
+        </section>
+        <section>
+          <Button
+            onClick={() => console.log(mapStateToJson(this.state))}
+            primary
+          >
+            Dodaj przepis
+          </Button>
+        </section>
       </Card>
     );
   }
