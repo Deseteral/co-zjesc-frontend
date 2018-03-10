7b5136934ac056d7dba3af3a13367a30a19e49c2
Deseteral <deseteral@gmail.com>
2018-03-10 19:18:52 +0100

    Title and description

--- src/pages/RecipeEditPage/RecipeEditPage.jsx
+++ src/pages/RecipeEditPage/RecipeEditPage.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import RichTextEditor from 'react-rte';
 import Card from '../../components/Card/Card';
+import CardHeader from '../../components/CardHeader/CardHeader';
+import TextField from '../../components/TextField/TextField';
 
 class RecipeEditPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
+      description: RichTextEditor.createEmptyValue(),
     };
   }
 
   render() {
     return (
       <Card>
-        RecipeEditPage
+        <CardHeader>Dodaj nowy przepis</CardHeader>
+        <TextField
+          name="title"
+          value={this.state.title}
+          placeholder="Tytuł przepisu"
+          onChange={value => this.handleChange(value, 'title')}
+        />
+        <RichTextEditor
+          value={this.state.description}
+          onChange={value => this.setState({ description: value })}
+        />
       </Card>
     );
   }
