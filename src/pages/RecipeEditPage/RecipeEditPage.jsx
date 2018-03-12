107b448720bcb96376fe9cef2b067774dd8e0e94
Deseteral <deseteral@gmail.com>
2018-03-12 23:49:55 +0100

    Attributes

--- src/pages/RecipeEditPage/RecipeEditPage.jsx
+++ src/pages/RecipeEditPage/RecipeEditPage.jsx
@@ -7,8 +7,23 @@ import Button from '../../components/Button/Button';
 import Select from '../../components/Select/Select';
 import styles from './RecipeEditPage.css';
 
+const DIFFICULTY_LEVELS = [
+  { value: 1, name: 'bardzo łatwy' },
+  { value: 2, name: 'łatwy' },
+  { value: 3, name: 'średni' },
+  { value: 4, name: 'trudny' },
+  { value: 5, name: 'bardzo trudny' },
+];
+
 function mapStateToJson(state) {
-  const { title, description } = state;
+  const {
+    title,
+    description,
+    difficulty,
+    estimatedCost,
+    portionCount,
+    timeToPrepare,
+  } = state;
 
   const products = state.products
     .filter(p => (p.name.length > 0 && !!p.unit))
@@ -16,8 +31,13 @@ function mapStateToJson(state) {
 
   return {
     title,
+    images: [],
     products,
     description: description.toString('markdown'),
+    difficulty,
+    estimatedCost,
+    portionCount,
+    timeToPrepare,
   };
 }
 
@@ -90,6 +110,45 @@ class RecipeEditPage extends Component {
             value={this.state.description}
             onChange={value => this.setState({ description: value })}
           />
+        </section>
+        <section>
+          <CardHeader secondary>Atrybuty</CardHeader>
+          <ul>
+            <li>
+              <span>Poziom trudności</span>
+              <Select
+                name="difficulty"
+                options={DIFFICULTY_LEVELS}
+                onChange={value => this.handleChange(value, 'difficulty')}
+              />
+            </li>
+            <li>
+              <TextField
+                name="estimatedCost"
+                value={this.state.estimatedCost}
+                placeholder="Przybliżony koszt"
+                onChange={value => this.handleChange(value, 'estimatedCost')}
+              />
+              zł
+            </li>
+            <li>
+              <TextField
+                name="portionCount"
+                value={this.state.portionCount}
+                placeholder="Liczba porcji"
+                onChange={value => this.handleChange(value, 'portionCount')}
+              />
+            </li>
+            <li>
+              <TextField
+                name="timeToPrepare"
+                value={this.state.timeToPrepare}
+                placeholder="Czas przygotowania"
+                onChange={value => this.handleChange(value, 'timeToPrepare')}
+              />
+              minut
+            </li>
+          </ul>
         </section>
         <section>
           <Button
