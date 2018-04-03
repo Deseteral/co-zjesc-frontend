39d43120590abf3df6a6b50034ec66d05d091b63
Deseteral <deseteral@gmail.com>
2018-04-03 11:24:02 +0200

    Broken image icon

--- src/components/RecipeListingItem/RecipeListingItem.jsx
+++ src/components/RecipeListingItem/RecipeListingItem.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import Icon from 'material-ui/Icon';
 import styles from './RecipeListingItem.css';
 
 function RecipeListingItem({ recipe }) {
   return (
     <NavLink to={`/recipe/${recipe.id}`} className={styles['item']}>
-      <div
-        className={styles['image']}
-        style={({ backgroundImage: `url("${recipe.image}")` })}
-      />
+      {recipe.image && (
+        <div
+          className={styles['image']}
+          style={({ backgroundImage: `url("${recipe.image}")` })}
+        />
+      )}
+      {!recipe.image && (
+        <div className={styles['image']}>
+          <Icon>broken_image</Icon>
+        </div>
+      )}
       <div className={styles['text']}>
         {recipe.title}
       </div>
