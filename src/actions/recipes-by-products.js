e09781b53d0ecb736cc2ca2d047ca6d693f7f956
Deseteral <deseteral@gmail.com>
2018-05-19 17:10:26 +0200

    Documented actions

--- src/actions/recipes-by-products.js
+++ src/actions/recipes-by-products.js
@@ -1,7 +1,16 @@
+/**
+ * @module actions/recipes-by-products
+ */
+
 const RECIPES_BY_PRODUCTS_FETCH_REQUESTED = 'RECIPES_BY_PRODUCTS_FETCH_REQUESTED';
 const RECIPES_BY_PRODUCTS_FETCH_SUCCEEDED = 'RECIPES_BY_PRODUCTS_FETCH_SUCCEEDED';
 const RECIPES_BY_PRODUCTS_FETCH_FAILED = 'RECIPES_BY_PRODUCTS_FETCH_FAILED';
 
+/**
+ * Creates redux action that represents recipes fetching request
+ * @param {number[]} productIds - ids of products that were selected
+ * @returns redux standard action
+ */
 function recipesByProductsFetch(productIds) {
   return {
     type: RECIPES_BY_PRODUCTS_FETCH_REQUESTED,
@@ -9,6 +18,11 @@ function recipesByProductsFetch(productIds) {
   };
 }
 
+/**
+ * Creates redux action that represents successful recipes fetching
+ * @param {object[]} recipes - recipes that were fetched
+ * @returns redux standard action
+ */
 function recipesByProductsFetchSucceeded(recipes) {
   return {
     type: RECIPES_BY_PRODUCTS_FETCH_SUCCEEDED,
@@ -16,6 +30,11 @@ function recipesByProductsFetchSucceeded(recipes) {
   };
 }
 
+/**
+ * Creates redux action that represents failed recipes fetching
+ * @param {string} message - error message
+ * @returns redux standard action
+ */
 function recipesByProductsFetchFailed(message) {
   return {
     type: RECIPES_BY_PRODUCTS_FETCH_FAILED,
--- src/actions/selected-products.js
+++ src/actions/selected-products.js
@@ -1,6 +1,15 @@
+/**
+ * @module actions/selected-products
+ */
+
 const SELECTED_PRODUCTS_ADD_PRODUCT = 'SELECTED_PRODUCTS_ADD_PRODUCT';
 const SELECTED_PRODUCTS_REMOVE_PRODUCT = 'SELECTED_PRODUCTS_REMOVE_PRODUCT';
 
+/**
+ * Creates redux action that represents adding new product to the list
+ * @param {object} product - recipe product
+ * @returns redux standard action
+ */
 function addProduct(product) {
   return {
     type: SELECTED_PRODUCTS_ADD_PRODUCT,
@@ -8,6 +17,11 @@ function addProduct(product) {
   };
 }
 
+/**
+ * Creates redux action that represents removing product from the list
+ * @param {number} productId - recipe product id
+ * @returns redux standard action
+ */
 function removeProduct(productId) {
   return {
     type: SELECTED_PRODUCTS_REMOVE_PRODUCT,
