db97cee5ca3154b217dfebdc1f10de1181b89720
Deseteral <deseteral@gmail.com>
2018-05-19 17:05:04 +0200

    Initial JSDoc

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,13 +1,26 @@
+/**
+ * @module actions/products
+ */
+
 const PRODUCTS_FETCH_REQUESTED = 'PRODUCTS_FETCH_REQUESTED';
 const PRODUCTS_FETCH_SUCCEEDED = 'PRODUCTS_FETCH_SUCCEEDED';
 const PRODUCTS_FETCH_FAILED = 'PRODUCTS_FETCH_FAILED';
 
+/**
+ * Creates redux action that represents products fetching request
+ * @returns redux standard action
+ */
 function productsFetch() {
   return {
     type: PRODUCTS_FETCH_REQUESTED,
   };
 }
 
+/**
+ * Creates redux action that represents successful products fetch
+ * @param {object[]} products - list of products that were fetched
+ * @returns redux standard action
+ */
 function productsFetchSucceeded(products) {
   return {
     type: PRODUCTS_FETCH_SUCCEEDED,
@@ -15,6 +28,11 @@ function productsFetchSucceeded(products) {
   };
 }
 
+/**
+ * Creates redux action that represents failed products fetch
+ * @param {string} message - error message
+ * @returns redux standard action
+ */
 function productsFetchFailed(message) {
   return {
     type: PRODUCTS_FETCH_FAILED,
